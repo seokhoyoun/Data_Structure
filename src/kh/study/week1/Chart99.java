abdf8e502c0c8ee1baed0a24a16306cbec04b194
Seokho Youn <axlover@hotmail.com>
2018-12-14 01:05:04 +0900

    init

---
+++ src/kh/study/week1/Chart99.java
@@ -0,0 +1,24 @@
+package kh.study.week1;
+
+public class Chart99 {
+
+	public static void main(String[] args) {
+		for(int i  = 1; i < 10; i++) {
+			if(i == 1) {
+				for(int p = 0; p < 10; p++) {
+					if(p == 0) {
+						System.out.print("   |\t");continue;
+					}
+					System.out.print(i*p+"\t");
+				}System.out.println("\n---+---------------------------------------------------------------------");
+			}
+			for(int j = 1; j < 10; j++) {
+				if(j == 1) {
+					System.out.print(i+"  |\t");
+				}
+				System.out.print(i*j+"\t");
+			}System.out.println();
+		}
+	}
+
+}
