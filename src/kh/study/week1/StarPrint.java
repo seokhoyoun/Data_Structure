9cc178d3da1b72cf9168585338158267a3dc0300
Seokho Youn <axlover@hotmail.com>
2018-12-16 12:04:11 +0900

    StarPrint 구현

---
+++ src/kh/study/week1/StarPrint.java
@@ -0,0 +1,66 @@
+package kh.study.week1;
+
+import java.util.Scanner;
+
+public class StarPrint {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("별 사각형의 크기 입력 : ");
+//		TriangleLB(sc.nextInt());
+//		TriangleLU(sc.nextInt());
+//		TriangleRU(sc.nextInt());
+		TriangleRB(sc.nextInt());
+	}
+	
+	static void StarSquare(int count) {		//  입력 받은 숫자 크기만큼 사각형을 만들어 출력
+		for(int i = 0; i < count; i++) {
+			for(int j = 0; j < count; j++) {
+				System.out.print("★");
+			}System.out.println();
+		}
+	}
+	
+	static void TriangleLB(int count) {		
+		for(int i = 0; i < count; i++) {
+			for(int j = 0; j < i+1; j++) {		// 별의 수가 하나씩 늘어나도록 구현
+				System.out.print("★");
+			}System.out.println();
+		}
+	}
+	
+	static void TriangleLU(int count) {
+		for(int i = 0; i < count; i++) {
+			for(int j = 0; j < count-i; j++) {	//	별의 수가 하나씩 줄어들도록 구현 
+				System.out.print("★");
+			}System.out.println();
+		}
+	}
+	
+	static void TriangleRU(int count) {
+		for(int i = 0; i < count; i++) {	
+			for(int k = 0; k < i; k++) {	// 	공백 문자가 하나씩 늘어나면서 별보다 먼저 출력 
+				System.out.print("☆"); // 공백 문자
+			}
+			
+			for(int j = 0; j < count-i; j++) {	//	별의 수가 하나씩 줄어들도록 구현
+				System.out.print("★");
+			}System.out.println();
+		}
+	}
+	
+	static void TriangleRB(int count) {
+		for(int i = 0; i < count; i++) {
+			for(int k = 0; k < count-(i+1); k++) {	// 공백 문자가 하나씩 줄어들면서 별보다 먼저 출력.
+				System.out.print("☆");
+			}
+			
+			for(int j = 0; j < i+1; j++) {	// 별의 수가 하나씩 늘어나도록 구현
+				System.out.print("★");
+			}System.out.println();
+		}
+	}
+	
+	
+
+}
